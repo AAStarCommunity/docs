8cd318bea0069240f4247dbdf7f98db65a487e29
jhfnetboy <jhfnetboy@gmail.com>
2025-12-26 13:13:49 +0700

    docs: include time in last updated timestamp

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -117,7 +117,7 @@ export default defineConfig({
     ],
 
     footer: {
-      message: 'Released under the MIT License. | Last Updated: 2025-12-26',
+      message: 'Released under the MIT License. | Last Updated: 2025-12-26 13:13',
       copyright: 'Copyright © 2024-present AAStar Community'
     },
 
